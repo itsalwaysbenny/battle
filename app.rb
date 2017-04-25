cd3c9cdce6cada32e32c08e014f848d35bbb4277
Paul Kassar <paulkassar@gmail.com>
2017-04-25 17:54:44 +0100

    Sessions enabled, now storing names in session. Passing rspec

--- app.rb
+++ app.rb
@@ -1,15 +1,21 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
+
   get '/' do
-  erb :index
+    erb :index
+  end
 
-end
+  post '/names' do
+    session[:name1] = params[:name1]
+    session[:name2] = params[:name2]
+    redirect '/play'
+  end
 
-
-post '/names' do
-@name1 = params[:name1]
-@name2 = params[:name2]
-erb :play
-end
+  get '/play' do
+    @name1 = session[:name1]
+    @name2 = session[:name2]
+    erb :play
+  end
 end
